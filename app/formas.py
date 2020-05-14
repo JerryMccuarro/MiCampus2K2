ed4746b3ea22ac523c3182cf6cb77d99b4a825ba
Norza <david.norzagaray@cetys.edu.mx>
2020-05-14 11:20:47 -0700

    Cambie etiquetas a ingles, jejeje

--- app/formas.py
+++ app/formas.py
@@ -3,11 +3,11 @@ from wtforms import StringField, IntegerField, SelectField, SubmitField
 from wtforms.validators import DataRequired,Length, NumberRange
 
 class FormaAlta(FlaskForm):
-    nombre = StringField("Nombre", validators=[DataRequired(message="Este campo en obligatorio.")])
-    apellido = StringField("Apellido", validators=[DataRequired(message="Este campo en obligatorio.")])
-    matricula = IntegerField("Matricula",validators=[DataRequired(message="Este campo en obligatorio.")])
-    materia = SelectField("Materia",validators=[DataRequired(message="Este campo en obligatorio.")],choices=[("POO","Programacion orientada a objetos"),
+    nombre = StringField("Name", validators=[DataRequired(message="Este campo en obligatorio.")])
+    apellido = StringField("Last Name", validators=[DataRequired(message="Este campo en obligatorio.")])
+    matricula = IntegerField("Student ID",validators=[DataRequired(message="Este campo en obligatorio.")])
+    materia = SelectField("Course",validators=[DataRequired(message="Este campo en obligatorio.")],choices=[("POO","Programacion orientada a objetos"),
                                                                          ("esp","Español"),
                                                                          ("mate","Matematicas")])
-    calificacion = IntegerField("Calificacion",validators=[DataRequired(message="Este campo en obligatorio."),NumberRange(min=0,max=100,message="Solo entre 0 y 100")])
+    calificacion = IntegerField("Grade",validators=[DataRequired(message="Este campo en obligatorio."),NumberRange(min=0,max=100,message="Solo entre 0 y 100")])
     guardar = SubmitField("Guardar")
